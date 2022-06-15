bfea8a1db389cf9fa1a1f3ee9bdd8f0c78404027
Nancyinfantina44 <102396690+Nancyinfantina44@users.noreply.github.com>
2022-06-15 07:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ server = app.server
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+fig = px.bar(df, x="gdp per capita", y="life expectancy",
                  size="population", color="continent", hover_name="country",
                  log_x=True, size_max=60)
 
